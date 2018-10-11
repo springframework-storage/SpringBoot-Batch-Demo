164718dca4be3445cd769aca7919b1f6293672a1
MinGOODdev <mingood92@gmail.com>
2018-10-12 01:06:24 +0900

    Feat: Batch_Job_Instance
    - meta table

---
+++ README.md
@@ -0,0 +1,7 @@
+# SpringBoot-Batch
+
+## Spring Batch의 메타 테이블
+### Batch_Job_Instance
+* Job Parameter 에 따라 생성되는 테이블
+* Spring Batch 가 실행될 때, 외부에서 받을 수 있는 파라미터
+* 동일한 Job Parameter 는 여러 개 존재할 수 없다.
--- src/main/java/com/example/batch/job/SimpleJobConfiguration.java
+++ src/main/java/com/example/batch/job/SimpleJobConfiguration.java
@@ -5,8 +5,10 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.JobScope;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -32,16 +34,18 @@ public class SimpleJobConfiguration {
   public Job simpleJob() {
     // simpleJob 이라는 Batch Job을 생성
     return jobBuilderFactory.get("simpleJob")
-            .start(simpleStep1())
+            .start(simpleStep1(null))
             .build();
   }
 
   @Bean
-  public Step simpleStep1() {
+  @JobScope
+  public Step simpleStep1(@Value("#{jobParameters[requestDate]}") String requestDate) {
     // simpleStep1 이라는 Batch Step 생성
     return stepBuilderFactory.get("simpleStep1")
             .tasklet((contribution, chunkContext) -> {  // Step 안에서 수행될 기능 명시
               log.info(">>>>>> This is Step1");
+              log.info(">>>>>> requestDate = {}", requestDate);
               return RepeatStatus.FINISHED;
             })
             .build();
