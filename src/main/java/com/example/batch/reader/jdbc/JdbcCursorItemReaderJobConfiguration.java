31b276e1a213404b895541a2137fb7f726742926
MinGOODdev <mingood92@gmail.com>
2018-10-16 20:53:50 +0900

    Feat: ItemReader
    - JdbcCursorItemReader
    - JdbcPagingItemReader
    - JpaPagingItemReader

---
+++ src/main/java/com/example/batch/reader/jdbc/JdbcCursorItemReaderJobConfiguration.java
@@ -0,0 +1,110 @@
+package com.example.batch.reader.jdbc;
+
+import com.example.batch.domain.Pay;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.database.JdbcCursorItemReader;
+import org.springframework.batch.item.database.builder.JdbcCursorItemReaderBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+
+import javax.sql.DataSource;
+
+/**
+ * reader 는 Tasklet 이 아니기 때문에 reader 만으로는 수행될 수 없고, 간단한 출력 Writer 를 하나 추가합니다.
+ * Processor 는 필수가 아닙니다.
+ * - reader 에서 읽은 데이터에 대해 크기 변경 로직이 없다면 processor 를 제외하고 writer 만 구현하면 됩니다.
+ */
+
+@Slf4j
+@RequiredArgsConstructor
+@Configuration
+public class JdbcCursorItemReaderJobConfiguration {
+
+  private final JobBuilderFactory jobBuilderFactory;
+  private final StepBuilderFactory stepBuilderFactory;
+  private final DataSource dataSource;
+
+  private int chunkSize = 10;
+
+  @Bean
+  public Job jdbcCursorItemReaderJob() {
+    return jobBuilderFactory.get("jdbcCursorItemReaderJob")
+            .start(jdbcCursorItemReaderStep())
+            .build();
+  }
+
+  @Bean
+  public Step jdbcCursorItemReaderStep() {
+    return stepBuilderFactory.get("jdbcCursorItemReaderStep")
+            /**
+             * 첫 번째 Pay 는 Reader 에서 반환할 타입,
+             * 두 번째 Pay 는 Writer 에 파라미터로 넘어올 타입
+             * chunkSize 로 인자값을 넣는 것은 Reader & Writer 가 묶을 Chunk 트랜잭션 범위
+             */
+            .<Pay, Pay>chunk(chunkSize)
+            .reader(jdbcCursorItemReader())
+            .writer(jdbcCursorItemWriter())
+            .build();
+  }
+
+  /**
+   * 아래 코드를 jdbcTemplate 로 구현하면 다음과 같다
+   *
+   * JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
+   * List<Pay> payList = jdbcTemplate.query("SELECT id, amount, tx_name, tx_date_time FROM pay", new BeanPropertyRowMapper<>(Pay.class));
+   *
+   * 거의 차이가 없지만, ItemReader 의 가장 큰 장점은 데이터를 Streaming 할 수 있다는 것
+   * read() 메소드는 데이터를 하나씩 가져와 ItemWriter 로 데이터를 전달하고, 다음 데이터를 다시 가져온다.
+   * 이를 통해 reader & processor & writer 가 Chunk 단위로 수행되고 주기적으로 Commit 된다.
+   * 이는 고성능의 배치 처리에서 핵심이다.
+   *
+   * @return
+   */
+  @Bean
+  public JdbcCursorItemReader<Pay> jdbcCursorItemReader() {
+    return new JdbcCursorItemReaderBuilder<Pay>()
+            /**
+             * fetchSize: DB 에서 한번에 가져올 데이터의 양
+             * Paging 과는 다른 것이 Paging 은 실제 쿼리에 limit, offset 을 이용해 분할 처리
+             * 반면, Cursor 의 쿼리는 분할 처리 없이 실행되나 내부적으로 가져오는 데이터는 Fetch Size 만큼 가져와
+             * read() 를 통해 하나씩 가져온다.
+             */
+            .fetchSize(chunkSize)
+            /**
+             * DB 에 접근하기 위해 사용할 DataSource 객체를 할당
+             */
+            .dataSource(dataSource)
+            /**
+             * 쿼리 결과를 Java 인스턴스로 맵핑하기 위한 Mapper
+             * 커스텀하게 생성해서 사용할 수도 있지만, 그럴 경우 매번 Mapper 클래스를 생성해야 하기 떄문에
+             * 보편적으로는 Spring 에서 공식적으로 지원하는 BeanPropertyRowMapper.class 를 많이 사용한다.
+             */
+            .rowMapper(new BeanPropertyRowMapper<>(Pay.class))
+            /**
+             * Reader 로 사용할 쿼리문
+             */
+            .sql("SELECT id, amount, tx_name, tx_date_time FROM pay")
+            /**
+             * reader 의 이름 지정
+             * Bean 의 이름이 아니며, Spring Batch 의 ExecutionContext 에서 저장되어질 이름
+             */
+            .name("jdbcCursorItemReader")
+            .build();
+  }
+
+  private ItemWriter<Pay> jdbcCursorItemWriter() {
+    return list -> {
+      for (Pay pay : list) {
+        log.info("** Current Pay = {}", pay);
+      }
+    };
+  }
+
+}
---
+++ src/main/java/com/example/batch/reader/jdbc/JdbcPagingItemReaderJobConfiguration.java
@@ -0,0 +1,117 @@
+package com.example.batch.reader.jdbc;
+
+import com.example.batch.domain.Pay;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.database.JdbcPagingItemReader;
+import org.springframework.batch.item.database.Order;
+import org.springframework.batch.item.database.PagingQueryProvider;
+import org.springframework.batch.item.database.builder.JdbcPagingItemReaderBuilder;
+import org.springframework.batch.item.database.support.SqlPagingQueryProviderFactoryBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+
+import javax.sql.DataSource;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * JdbcCursorItemReader 와 설정이 크게 다른 것: createQueryProvider()
+ * - JdbcCursorItemReader 는 단순히 String 타입으로 쿼리를 생성
+ * - PagingItemReader 는 PagingQueryProvider 를 통해 쿼리를 생성
+ * 이렇게 하는 데는 큰 이유가 있다.
+ *
+ * 각 DB 에는 Paging 을 지원하는 자체적인 전략들이 존재한다.
+ * 그렇기 떄문에 Spring Batch 는 각 DB 의 Paging 전략에 맞춰 구현되어야만 합니다.
+ * - ex) MySqlPagingQueryProvider
+ * - ex) OraclePagingQueryProvider
+ * - ex) PostgresPagingQueryProvider
+ * - ex) SqlitePagingQueryProvider
+ *
+ * 하지만 이렇게 되면 DB 마다 Provider 코드를 바꿔야 한다는 불편함이 존재한다.
+ * 그래서 Spring Batch 에서는 SqlPagingQueryProviderFactoryBean 을 통해 DataSource 설정값을 보고
+ * DB Provider 중 하나를 자동으로 선택한다. (Spring Batch 에서 공식 지원하는 방법)
+ */
+
+@Slf4j
+@RequiredArgsConstructor
+@Configuration
+public class JdbcPagingItemReaderJobConfiguration {
+
+  private final JobBuilderFactory jobBuilderFactory;
+  private final StepBuilderFactory stepBuilderFactory;
+  private final DataSource dataSource;
+
+  private int chunkSize = 10;
+
+  @Bean
+  public Job jdbcPagingItemReaderJob() throws Exception {
+    return jobBuilderFactory.get("jdbcPagingItemReaderJob")
+            .start(jdbcPagingItemReaderStep())
+            .build();
+  }
+
+  @Bean
+  public Step jdbcPagingItemReaderStep() throws Exception {
+    return stepBuilderFactory.get("jdbcPagingItemReaderStep")
+            .<Pay, Pay>chunk(chunkSize)
+            .reader(jdbcPagingItemReader())
+            .writer(jdbcPagingItemWriter())
+            .build();
+  }
+
+  @Bean
+  public JdbcPagingItemReader<Pay> jdbcPagingItemReader() throws Exception {
+    Map<String, Object> parameterValues = new HashMap<>();
+    parameterValues.put("amount", 2000);
+
+    return new JdbcPagingItemReaderBuilder<Pay>()
+            .fetchSize(chunkSize)
+            .dataSource(dataSource)
+            .rowMapper(new BeanPropertyRowMapper<>(Pay.class))
+            .queryProvider(createQueryProvider())
+            /**
+             * 쿼리에 대한 매개 변수 값의 Map 을 지정
+             * queryProvider.setWhereClause 를 보면 어떻게 변수를 사용하는 지 알 수 있다.
+             * where 절에서 선언된 파라미터 변수명과 parameterValues 에서 선언된 파라미터 변수명이 일치해야 한다.
+             *
+             * 예전이 '?' 로 파라미터의 위치를 지정하고 1 부터 시작하여 각 파라미터 값을 할당시키는 방식이었다면,
+             * 그에 비해 굉장히 명시적이고 실수할 여지가 줄어들었다.
+             *
+             */
+            .parameterValues(parameterValues)
+            .name("jdbcPagingItemReader")
+            .build();
+  }
+
+  private ItemWriter<Pay> jdbcPagingItemWriter() {
+    return list -> {
+      for (Pay pay : list) {
+        log.info("** Current Pay = {}", pay);
+      }
+    };
+  }
+
+  @Bean
+  public PagingQueryProvider createQueryProvider() throws Exception {
+    SqlPagingQueryProviderFactoryBean queryProvider = new SqlPagingQueryProviderFactoryBean();
+    queryProvider.setDataSource(dataSource);  // DB 에 맞는 PagingQueryProvider 를 선택하기 위함
+    queryProvider.setSelectClause("id, amount, tx_name, tx_date_time");
+    queryProvider.setFromClause("from pay");
+    queryProvider.setWhereClause("where amount >= :amount");
+
+    Map<String, Order> sortKeys = new HashMap<>(1);
+    sortKeys.put("id", Order.ASCENDING);
+
+    queryProvider.setSortKeys(sortKeys);
+
+    return queryProvider.getObject();
+  }
+
+}
